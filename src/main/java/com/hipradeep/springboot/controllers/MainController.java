201b4e59451bba057dcbf50414a4fb6a477d3e41
pradeep <pradeepmaurya.cse@gmail.com>
2022-08-02 00:16:06 +0530

    update

---
+++ src/main/java/com/hipradeep/springboot/controllers/MainController.java
@@ -0,0 +1,23 @@
+package com.hipradeep.springboot.controllers;
+
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String home() {
+		
+		System.out.println("This is home");
+		return "home";
+	}
+	@RequestMapping("/contact")
+	public String contact() {
+		
+		System.out.println("This is home");
+		return "contact";
+	}
+
+}
